a19b267b593fb8677656f2d4f366411263f53ddc
Nick Ito <nick.yusuke.ito@gmail.com>
2024-05-27 09:46:20 -0700

    feat: add cors and json to express-boilerplate

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,14 @@
+import cors from "cors";
 import express from "express";
 
 export const app = express();
+
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+	})
+);
+app.use(express.json());
 
 app.get("/", (_, res) => {
 	res.send("Hello World!");
